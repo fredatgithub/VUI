bcab47030833d36dc29aa4611e735c56216e6c4d
vosongtoan <vosongtoan@outlook.com>
2024-05-14 05:33:00 -0700

    Add detailed documentation to InteractionState property and ContentType property to IUIElement interface

--- VUI/IUIElement.cs
+++ VUI/IUIElement.cs
@@ -12,7 +12,33 @@ namespace VUI
         //
         // Define the properties that all UI elements should have
         //
+
+        /// <summary>
+        /// Represents the interaction state of a UI element. 
+        /// The possible states are:
+        /// <list type="bullet">
+        /// <item>
+        /// <description>"Normal": The default state of the UI element.</description>
+        /// </item>
+        /// <item>
+        /// <description>"Clicked": The state when the UI element is clicked.</description>
+        /// </item>
+        /// <item>
+        /// <description>"Toggled": The state when the UI elemnt is toggled on or off.</description>
+        /// </item>
+        /// <item>
+        /// <description>"MouseEnter": The state when the mouse pointer enters the UI element.</description>
+        /// </item>
+        /// <item>
+        /// <description>"MouseLeave": The state when the mouse pointer leaves the UI element.</description>
+        /// </item>
+        /// </list>
+        /// </summary>
         string InteractionState { get; set; }
+
+
+        string ContentType { get; set; }
+
 
         //
         // Define the methods that all UI elements should implement
