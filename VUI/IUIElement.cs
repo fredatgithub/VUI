107bc5d5cc52680b7f0ed9f66ec3a36724969b3b
vosongtoan <vosongtoan@outlook.com>
2024-05-14 12:27:06 -0700

    Add more state-colors and Transition system

--- VUI/IUIElement.cs
+++ VUI/IUIElement.cs
@@ -40,11 +40,34 @@ namespace VUI
         string ContentType { get; set; }
 
 
+        public string MouseEnter_BackgroundColor { get; set; }
+        public string MouseLeave_BackgroundColor { get; set; }
+        public string Normal_BackgroundColor { get; set; }
+        public string Clicked_BackgroundColor { get; set; }
+        public string Toggled_BackgroundColor { get; set; }
+
+        public string BackgroundColor { get; set; }
+
+        bool IsTransitioned { get; set; }
+
+
         //
         // Define the methods that all UI elements should implement
         //
         Task InternalOnClick();
         Task InternalOnMouseEnter();
         Task InternalOnMouseLeave();
+
+
+        /// <summary>
+        /// Invoking this function changes the InteractionState to the provided _interactionState.
+        /// It also prevents further changes to the InteractionState by users.
+        /// Developers can resume changes to the InteractionState by calling the StopTransition() method.
+        /// </summary>
+        /// <param name="_interactionState">The state to which the InteractionState will transition.</param>
+        /// <returns>A Task representing the asynchronous operation.</returns>
+        Task TransitionTo(string _interactionState);
+
+        void StopTransition();
     }
 }
