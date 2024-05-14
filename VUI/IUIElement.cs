fc8b9dd691968bee34ff1e8bd7a7e03a84f159ef
vosongtoan <vosongtoan@outlook.com>
2024-05-14 03:01:27 -0700

    Implement IUIElement interface across all VUI Elements

---
+++ VUI/IUIElement.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VUI
+{
+    internal interface IUIElement
+    {
+        //
+        // Define the properties that all UI elements should have
+        //
+        string InteractionState { get; set; }
+
+        //
+        // Define the methods that all UI elements should implement
+        //
+        Task InternalOnClick();
+        Task InternalOnMouseEnter();
+        Task InternalOnMouseLeave();
+    }
+}
