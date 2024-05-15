220af62b4aa2803abcd0751556df15da64a682b8
vosongtoan <vosongtoan@outlook.com>
2024-05-15 08:11:32 -0700

    Refactor TransitionManager.Handle to improve code readability and manageability.

--- VUI/TransitionManager.cs
+++ VUI/TransitionManager.cs
@@ -15,30 +15,35 @@ namespace VUI
             {
                 case "UIState":
 
-                    switch (e.Transition)
-                    {
-                        case "Color":
-
-                            switch (e.InteractionState + "_BackgroundColor")
-                            {
-                                case "Normal_BackgroundColor":
-                                    e.BackgroundColor = e.Normal_BackgroundColor;
-                                    break;
-
-                                case "MouseEnter_BackgroundColor":
-                                    e.BackgroundColor = e.MouseEnter_BackgroundColor;
-                                    break;
-
-                                case "Clicked_BackgroundColor":
-                                    e.BackgroundColor = e.Clicked_BackgroundColor;
-                                    break;
-                            }
-                            break;
-                    }
+                    Handle_UIState(e);
 
                     break;
 
                 case "UserDecision":
+                    break;
+            }
+        }
+
+        private static void Handle_UIState(IUIElement e)
+        {
+            switch (e.Transition)
+            {
+                case "Color":
+
+                    switch (e.InteractionState + "_BackgroundColor")
+                    {
+                        case "Normal_BackgroundColor":
+                            e.BackgroundColor = e.Normal_BackgroundColor;
+                            break;
+
+                        case "MouseEnter_BackgroundColor":
+                            e.BackgroundColor = e.MouseEnter_BackgroundColor;
+                            break;
+
+                        case "Clicked_BackgroundColor":
+                            e.BackgroundColor = e.Clicked_BackgroundColor;
+                            break;
+                    }
                     break;
             }
         }
