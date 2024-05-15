9c106ee02e878445e2d2733e273e0dc283744c27
vosongtoan <vosongtoan@outlook.com>
2024-05-15 03:12:41 -0700

    Add a new class named TransitionManager

---
+++ VUI/TransitionManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.TimeZoneInfo;
+
+namespace VUI
+{
+    public static class TransitionManager
+    {
+        internal static void Handle(IUIElement e)
+        {
+            switch (e.TransitionType)
+            {
+                case "UIState":
+
+                    switch (e.Transition)
+                    {
+                        case "Color":
+
+                            switch (e.InteractionState + "BackgroundColor")
+                            {
+                                case "Normal_BackgroundColor":
+                                    e.BackgroundColor = e.Normal_BackgroundColor;
+                                    break;
+
+                                case "MouseEnter_BackgroundColor":
+                                    e.BackgroundColor = e.MouseEnter_BackgroundColor;
+                                    break;
+
+                                case "Clicked_BackgroundColor":
+                                    e.BackgroundColor = e.Clicked_BackgroundColor;
+                                    break;
+                            }
+                            break;
+                    }
+
+                    break;
+
+                case "UserDecision":
+                    break;
+            }
+        }
+    }
+}
