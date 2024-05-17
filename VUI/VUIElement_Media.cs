de7a156b1155957ba5efff742668302293429637
vosongtoan <vosongtoan@outlook.com>
2024-05-17 13:17:32 -0700

    Add PlayVideo and PauseVideo functions in VUIElement_Media.cs, and its implementation in VUIElement.razor

---
+++ VUI/VUIElement_Media.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VUI
+{
+    public partial class VUIElement : ComponentBase, IUIElement
+    {
+        public async Task PlayVideo()
+        {
+            await JSRuntime.InvokeVoidAsync("playVideo", MediaID);
+        }
+
+        public async Task PauseVideo()
+        {
+            await JSRuntime.InvokeVoidAsync("pauseVideo", MediaID);
+        }
+    }
+}
