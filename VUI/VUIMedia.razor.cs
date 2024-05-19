6f07b05e3bf5fbb48fcff879a62479f157545143
vosongtoan <vosongtoan@outlook.com>
2024-05-19 06:49:14 -0700

    Move InternalOnPlay and InternalOnTimeUpdate methods from VUIMedia.razor.cs to VUIMedia_EventHandlers.cs

--- VUI/VUIMedia.razor.cs
+++ VUI/VUIMedia.razor.cs
@@ -55,42 +55,6 @@ namespace VUI
             }
         }
 
-        public override async Task InternalOnPlay()
-        {
-            switch (ContentType)
-            {
-                case "Audio":
-                case "Video":
-
-                    CurrentTime = await JSRuntime.InvokeAsync<double>(
-                        "getMediaCurrentTime", MediaID);
-
-                    if (OnCurrentTimeUpdate.HasDelegate)
-                    {
-                        await OnCurrentTimeUpdate.InvokeAsync(this);
-                    }
-
-                    break;
-            }
-        }
-
-        public override async Task InternalOnTimeUpdate()
-        {
-            switch (ContentType)
-            {
-                case "Audio":
-                case "Video":
-
-                    CurrentTime = await JSRuntime.InvokeAsync<double>(
-                        "getMediaCurrentTime", MediaID);
-
-                    if (OnCurrentTimeUpdate.HasDelegate)
-                    {
-                        await OnCurrentTimeUpdate.InvokeAsync(this);
-                    }
-
-                    break;
-            }
-        }
+        
     }
 }
---
+++ VUI/VUIMedia_EventHandlers.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VUI
+{
+    public partial class VUIMedia : VUIElement
+    {
+        public override async Task InternalOnPlay()
+        {
+            switch (ContentType)
+            {
+                case "Audio":
+                case "Video":
+
+                    CurrentTime = await JSRuntime.InvokeAsync<double>(
+                        "getMediaCurrentTime", MediaID);
+
+                    if (OnCurrentTimeUpdate.HasDelegate)
+                    {
+                        await OnCurrentTimeUpdate.InvokeAsync(this);
+                    }
+
+                    break;
+            }
+        }
+
+        public override async Task InternalOnTimeUpdate()
+        {
+            switch (ContentType)
+            {
+                case "Audio":
+                case "Video":
+
+                    CurrentTime = await JSRuntime.InvokeAsync<double>(
+                        "getMediaCurrentTime", MediaID);
+
+                    if (OnCurrentTimeUpdate.HasDelegate)
+                    {
+                        await OnCurrentTimeUpdate.InvokeAsync(this);
+                    }
+
+                    break;
+            }
+        }
+    }
+}
