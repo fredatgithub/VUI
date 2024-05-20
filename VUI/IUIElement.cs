b79733f9743597cd0718cf48618454f87ce68a35
vosongtoan <vosongtoan@outlook.com>
2024-05-20 00:09:42 -0700

    Update TransitionType comments in IUIElement.cs to reflect correct MediaState value

--- VUI/IUIElement.cs
+++ VUI/IUIElement.cs
@@ -106,9 +106,9 @@ namespace VUI
         /// <description>"MediaState": This value is used when 
         /// a state change occurs in a Media element. 
         /// The possible state transitions for a Media element are as follows:
-        /// <para>Normal -> Play/PlayAt -> Pause -> Play -> Stop: 
+        /// <para>Normal -> Play/PlayAt -> Pause -> Play -> End: 
         /// This represents a typical sequence of states for a media playback scenario.</para>
-        /// <para>Normal -> Play/PlayAt -> Stop: 
+        /// <para>Normal -> Play/PlayAt -> End: 
         /// This represents a scenario where the media is played and 
         /// then stopped without being paused.</para>
         /// </description>
